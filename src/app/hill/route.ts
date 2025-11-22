be566996c62a6dc2c7ddd4abbba723e9743ad4f4
CoocooFroggy <45371102+CoocooFroggy@users.noreply.github.com>
2025-11-22 02:01:49 -0600

    Update docs

--- src/app/hill/route.ts
+++ src/app/hill/route.ts
@@ -2,9 +2,15 @@ import {NextRequest} from "next/server";
 
 let currentKing = "Nobody"
 
+
+// —— GET request (client will GET: who is on the hill?) ——
+
 export async function GET() {
   return Response.json({ king: currentKing })
 }
+
+
+// —— POST request (client will POST: I am the new king of the hill) ——
 
 type NewKingPost = {
   newKing: string,
